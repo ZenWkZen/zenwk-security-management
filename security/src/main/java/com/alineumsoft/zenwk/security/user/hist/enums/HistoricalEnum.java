f27219f918003deec9c30772ff8b6d63510a7170
calegria <calegria@MacBook-Pro-de-calegria.local>
2025-01-02 21:19:46 -0500

    V-02012025-4
    V-02012025-4

--- security/src/main/java/com/alineumsoft/zenwk/security/user/hist/enums/HistoricalEnum.java
+++ security/src/main/java/com/alineumsoft/zenwk/security/user/hist/enums/HistoricalEnum.java
@@ -1,8 +1,10 @@
 package com.alineumsoft.zenwk.security.user.hist.enums;
 
+/**
+ * @author <a href="mailto:alineumsoft@gmail.com">C. Alegria</a>
+ * @project SecurityUser
+ * @class HistoricalEnum
+ */
 public enum HistoricalEnum {
-	INSERT,
-	UPDATE,
-	DELETE;
+	INSERT, UPDATE, DELETE;
 }
-
--- security/src/main/java/com/alineumsoft/zenwk/security/user/util/component/AppContextHolderComponent.java
+++ security/src/main/java/com/alineumsoft/zenwk/security/user/util/component/AppContextHolderComponent.java
@@ -5,16 +5,37 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author <a href="mailto:alineumsoft@gmail.com">C. Alegria</a>
+ * @project SecurityUser
+ * @class AppContextHolderComponent
+ */
 @Component
 public class AppContextHolderComponent implements ApplicationContextAware {
 
 	private static ApplicationContext context;
 
+	/**
+	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+	 * @param applicationContext
+	 * @throws BeansException
+	 * @see org.springframework.context.ApplicationContextAware#setApplicationContext(org.springframework.context.ApplicationContext)
+	 */
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		context = applicationContext;
 	}
 
+	/**
+	 * <p>
+	 * <b> General </b> Recupera un bean a partir de un contexto
+	 * </p>
+	 * 
+	 * @author <a href="alineumsoft@gmail.com">C. Alegria</a>
+	 * @param <T>
+	 * @param beanClass
+	 * @return
+	 */
 	public static <T> T getBean(Class<T> beanClass) {
 		return context.getBean(beanClass);
 	}
